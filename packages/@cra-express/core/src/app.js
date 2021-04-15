bee3250c59fb096508187db1556e889ee036fa23
Ken Colton <kcolton@gmail.com>
2021-04-15 19:27:07 -0400

    allow passing existing express app
    otherwise there is currently no way to add middleware which needs to come first

--- packages/@cra-express/core/src/app.js
+++ packages/@cra-express/core/src/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const staticLoader = require('@cra-express/static-loader').default;
 const universalLoader = require('@cra-express/universal-loader').default;
 
-function createReactAppExpress (options) {
-  const app = express();
+function createReactAppExpress (options, app=null) {
+  if (!app) {
+    app = express();
+  }
   staticLoader(app, options);
   universalLoader(app, options);
   return app;
